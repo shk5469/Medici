0ada093a08e0c69e94df2399a9a0ded6335c7e16
shk5469 <shk5469@naver.com>
2023-06-21 09:40:22 +0900

    Medici

---
+++ Day03/Day03/EqualTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03
+{
+    class EqualTest
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Hello/DataType1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f1 = 0.1f;
+
+            for(int i = 0; i<10; i++)
+            {
+                f1 += 0.1f;
+            }
+
+            Console.WriteLine(f1);
+
+        }
+    }
+}
